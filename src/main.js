3f4b33cf5bcb13505de5a7e7399705e5c7a3de85
yanglongbo <yanglongbo@sohu.com>
2022-05-05 17:45:17 +0800

    fix:移动端click事件延迟问题

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import store from './store'
 import "amfe-flexible"
 
+//解决移动端click事件的300毫秒延迟问题
+import fastclick from 'fastclick'
+fastclick.attach(document.body)
+
 import Vant from 'vant';
 import 'vant/lib/index.css';
 Vue.use(Vant);
